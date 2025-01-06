e004d9d424f03aef1c9fff2c2526ef788955f204
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-06 21:06:11 +0800

    - update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,49 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include <boost/math/distributions/empirical_cumulative_distribution_function.hpp>
 #include <emscripten/bind.h>
 
 using namespace emscripten;
 
-template<unsigned int N> 
-struct Factorial {
-  static constexpr unsigned int value = N * Factorial<N - 1>::value;
-};
+// template<unsigned int N> 
+// struct Factorial {
+//   static constexpr unsigned int value = N * Factorial<N - 1>::value;
+// };
 
-// Base case via template specialization:
-template<>
-struct Factorial<0> {
-  static constexpr unsigned int value = 1;
-};
+// // Base case via template specialization:
+// template<>
+// struct Factorial<0> {
+//   static constexpr unsigned int value = 1;
+// };
 
-EMSCRIPTEN_BINDINGS(my_module) {
-    constant("Factorial_10", Factorial<10>::value);  // not constant
+// EMSCRIPTEN_BINDINGS(my_module) {
+//     constant("Factorial_10", Factorial<10>::value);  // not constant
+// }
+
+double random_boost_stuff() {
+    using boost::math::empirical_cumulative_distribution_function;
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::normal_distribution<double> dis(0, 1);
+    size_t n = 128;
+    std::vector<double> v(n);
+    for (size_t i = 0; i < n; ++i) {
+        v[i] = dis(gen);
+    }
+
+    auto ecdf = empirical_cumulative_distribution_function(std::move(v));
+    return ecdf(0.0);
 }
+
+
+#ifndef _EMSCRIPTEN_ 
+int main() {
+    std::cout << "Result ecdf(0.0): " << random_boost_stuff() << std::endl;
+    return 0;
+}
+#else  /*_EMSCRIPTEN_*/
+    EMSCRIPTEN_BINDINGS(my_module) {
+        function("random_boost_stuff", &random_boost_stuff);
+    }
+#endif
