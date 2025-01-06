ced39afcdf9b6c5693f15cf464de59687077bd5a
Dirk Arnez <smalldirkalex@gmail.com>
2025-01-06 21:19:23 +0800

    - update build-in-docker.sh

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -2,6 +2,7 @@
 cmake -G"Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_TOOLCHAIN_FILE="/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake" \
+    -DBOOST_DIR='/usr/include/' \
     -B./cmake-build && \
     cd cmake-build && \
     cmake --build . && \
